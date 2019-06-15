ed309678d19dd70f77453c6a63d30f70bd6ffeb4
Drozdov29 <shadow207@mail.ru>
2019-06-15 20:56:35 +0300

    JS - помоги пожалуйста
    1.  Почему при нажатии а кнопку "Поиск гостиницы в седоне" - он сразу уезжает вправо, а потом нормаьлно закрывается.
    2. Что надо чтоб он спрятался.
    3. Что прописать надо чтоб кнопка работала 2 раз (1 раз окно уезжает, 2 раз окно выезжает)
    4. При не введенных полях ввода -  окно смещается тоже вправо.

---
+++ js/main-form.js
@@ -0,0 +1,20 @@
+var link = document.querySelector(".search-button-hotel");
+var popup = document.querySelector(".form-wrapper");
+var form = document.querySelector(".adventage-booking");
+var a_date = popup.querySelector("[name=data-arriva]");
+var d_date = popup.querySelector("[name=data-departure]");
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  console.log("Человек нажал на кнопку");
+  popup.classList.add("popup-up");
+
+});
+
+form.addEventListener("submit", function (evt){
+  if (!a_date || !d_date) {
+    evt.preventDefault();
+    console.log("Нужно ввести даты заезда и выезда");
+    popup.classList.add("popup-error");
+
+}});
