401320288b1dbb7eada7f327b58dc014fc451adc
Drozdov29 <shadow207@mail.ru>
2019-06-16 20:41:25 +0300

    16.06.2019 Комплект для Защиты

--- js/main-form.js
+++ js/main-form.js
@@ -1,8 +1,10 @@
 var link = document.querySelector(".search-button-hotel");
 var popup = document.querySelector(".hotel-search");
-var form = document.querySelector(".adventage-booking");
-var a_date = popup.querySelector("[name=data-arriva]");
+
+var a_date = popup.querySelector("[name=data-arrival]");
 var d_date = popup.querySelector("[name=data-departure]");
+var adults_num = popup.querySelector("[name=adults-number]");
+var chil_num = popup.querySelector("[name=children-number]");
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -10,20 +12,17 @@ link.addEventListener("click", function (evt) {
 if(popup.classList.contains("popup-closed")){
    popup.classList.remove("popup-closed");
    popup.classList.add("popup-open");
+   popup.classList.remove("popup-error");
 }
 else {popup.classList.remove("popup-open");
       popup.classList.add("popup-closed");
 }
 });
 
-
-
-
-
 popup.addEventListener("submit", function (evt){
-  if (!a_date || !d_date) {
-    evt.preventDefault();
-    console.log("Нужно ввести даты заезда и выезда");
-    popup.classList.remove("popup-error");}
-    else {popup.classList.remove("popup-error")};
+if (!a_date.value || !d_date.value || !adults_num.value || !chil_num.value) {
+  evt.preventDefault();
+  console.log("Нужно ввести даты заезда и выезда");
+  popup.classList.add("popup-error");}
+else {popup.classList.remove("popup-error")};
 })
