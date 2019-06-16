f7bfb17090f7e0ff63ce64e4b38e1e1a2e247fc2
Drozdov29 <shadow207@mail.ru>
2019-06-16 15:19:13 +0300

    js 16.06.2019

--- js/main-form.js
+++ js/main-form.js
@@ -1,5 +1,5 @@
 var link = document.querySelector(".search-button-hotel");
-var popup = document.querySelector(".form-wrapper");
+var popup = document.querySelector(".hotel-search");
 var form = document.querySelector(".adventage-booking");
 var a_date = popup.querySelector("[name=data-arriva]");
 var d_date = popup.querySelector("[name=data-departure]");
@@ -7,17 +7,23 @@ var d_date = popup.querySelector("[name=data-departure]");
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   console.log("Человек нажал на кнопку");
-  popup.classList.add("popup-up");
-
+if(popup.classList.contains("popup-closed")){
+   popup.classList.remove("popup-closed");
+   popup.classList.add("popup-open");
+}
+else {popup.classList.remove("popup-open");
+      popup.classList.add("popup-closed");
+}
 });
 
-form.addEventListener("submit", function (evt){
+
+
+
+
+popup.addEventListener("submit", function (evt){
   if (!a_date || !d_date) {
     evt.preventDefault();
     console.log("Нужно ввести даты заезда и выезда");
-    popup.classList.add("popup-error");
-    else {
-
-    }
-
-}});
+    popup.classList.remove("popup-error");}
+    else {popup.classList.remove("popup-error")};
+})
