1be206b24c816e16f04a3e2d075d6d225ad66b61
Drozdov29 <shadow207@mail.ru>
2019-06-18 20:26:54 +0300

    18.06.2019 -  2 попытка

--- js/main-form.js
+++ js/main-form.js
@@ -6,9 +6,12 @@ var d_date = popup.querySelector("[name=data-departure]");
 var adults_num = popup.querySelector("[name=adults-number]");
 var chil_num = popup.querySelector("[name=children-number]");
 
+popup.classList.add("popup-closed");
+
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   console.log("Человек нажал на кнопку");
+
 if(popup.classList.contains("popup-closed")){
    popup.classList.remove("popup-closed");
    popup.classList.add("popup-open");
